b8f1961ad6a036b82ee998c16b332d1c29076e57
Emad Khan <emadkhanqai@gmail.com>
2022-07-01 02:34:49 +0500

    @ContentChild and ngAfterContentInit

--- src/app/auth-form/auth-form.component.ts
+++ src/app/auth-form/auth-form.component.ts
@@ -1,14 +1,37 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import {
+  AfterContentInit,
+  Component,
+  ContentChild,
+  EventEmitter,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { User } from './auth-form.interface';
+import { AuthRememberComponent } from './auth-remember.component';
 
 @Component({
   selector: 'auth-form',
   templateUrl: './auth-form.component.html',
   styleUrls: ['./auth-form.component.scss'],
 })
-export class AuthFormComponent implements OnInit {
+export class AuthFormComponent implements OnInit, AfterContentInit {
   @Output() submitted: EventEmitter<User> = new EventEmitter<User>();
+
+  showMessage: Boolean = false;
+
+  @ContentChild(AuthRememberComponent) remember!: AuthRememberComponent;
+
   constructor() {}
+
+  ngAfterContentInit() {
+    if(this.remember !== undefined) {
+      if(this.remember) {
+        this.remember.checked.subscribe((check: Boolean) => {
+          this.showMessage = check;
+        })
+      }
+    }
+  }
 
   onSubmit(value: User) {
     this.submitted.emit(value);
