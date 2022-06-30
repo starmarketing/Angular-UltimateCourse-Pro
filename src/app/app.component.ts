86648bae5de70ce881e137ff44ea8d0d17c70a09
Emad Khan <emadkhanqai@gmail.com>
2022-06-30 16:40:59 +0500

    First commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { User } from './auth-form/auth-form.interface';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'angular-ultimatecourse-pro';
+  createUser(user: User) {
+    console.log('Create account', user);
+  }
+
+  loginUser(user: User) {
+    console.log('Login', user);
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { AuthFormComponent } from './auth-form/auth-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AuthFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    CommonModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
