838ca8b60926a76cba85c8dd01cf3c592f842a49
Emad Khan <emadkhanqai@gmail.com>
2022-07-03 03:04:21 +0500

    Componentizing FormGroups

--- src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
+++ src/app/stock-inventory/containers/stock-inventory/stock-inventory.component.ts
@@ -1,16 +1,33 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'stock-inventory',
   templateUrl: './stock-inventory.component.html',
   styleUrls: ['./stock-inventory.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class StockInventoryComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  form: FormGroup = new FormGroup({
+    store: new FormGroup({
+      branch: new FormControl(''),
+      code: new FormControl(''),
+    }),
+    selector: new FormGroup({
+      product_id: new FormControl(),
+      quantity: new FormControl()
+    }),
+    stock: new FormArray([])
+  });
+
+
+  // stock-branch
+  // stock-products
+  // stock-selector
+  submit() {
+    if (this.form.valid) console.log(this.form.value);
   }
-
 }
--- src/app/stock-inventory/stock-inventory.module.ts
+++ src/app/stock-inventory/stock-inventory.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StockInventoryComponent } from './containers/stock-inventory/stock-inventory.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { StockBranchComponent } from './components/stock-branch/stock-branch.component';
+import { StockProductsComponent } from './components/stock-products/stock-products.component';
+import { StockSelectorComponent } from './components/stock-selector/stock-selector.component';
 
 @NgModule({
-  declarations: [StockInventoryComponent],
-  imports: [CommonModule],
+  declarations: [StockInventoryComponent, StockBranchComponent, StockProductsComponent, StockSelectorComponent],
+  imports: [CommonModule, ReactiveFormsModule],
   exports: [StockInventoryComponent],
 })
 export class StockInventoryModule {}
